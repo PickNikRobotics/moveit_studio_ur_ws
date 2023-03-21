35417249a5e3aef55074c9e0174798220e86ad50
Michael Wrock <mike.wrock@gmail.com>
2023-03-21 16:03:14 -0700

    added generic ur site config

---
+++ src/generic_ur_config/launch/robot_drivers_to_restart.launch.py
@@ -0,0 +1,12 @@
+# Copyright 2021 PickNik Inc.
+# All rights reserved.
+#
+# Unauthorized copying of this code base via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription()
