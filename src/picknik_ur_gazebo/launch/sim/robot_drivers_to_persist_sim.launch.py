1d072b84840f6216e3b9135a14532845329ca8be
Michael Wrock <mike.wrock@gmail.com>
2023-03-23 12:37:54 -0700

    added gazebo and mock hardware ur arms

---
+++ src/picknik_ur_gazebo/launch/sim/robot_drivers_to_persist_sim.launch.py
@@ -0,0 +1,21 @@
+# Copyright 2021 PickNik Inc.
+# All rights reserved.
+#
+# Unauthorized copying of this code base via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Mock the UR Dashboard Client
+    mock_dashboard_client = Node(
+        package="moveit_studio_ur_pstop_manager",
+        executable="mock_ur_dashboard_client_node",
+        name="dashboard_client",
+        output="screen",
+    )
+
+    return LaunchDescription([mock_dashboard_client])
