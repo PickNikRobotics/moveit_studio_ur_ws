82839898c343e0cd1face2ce6bb1abc4a5edb000
Michael Wrock <mike.wrock@gmail.com>
2023-03-30 18:31:46 -0700

    added site configs without submodule

---
+++ src/picknik_ur_base_config/launch/sim/hardware_sim.launch.py
@@ -0,0 +1,43 @@
+# Copyright 2023 PickNik Inc.
+# All rights reserved.
+#
+# Unauthorized copying of this code base via any medium is strictly prohibited.
+# Proprietary and confidential.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from moveit_studio_utils_py.system_config import (
+    SystemConfigParser,
+)
+
+from moveit_studio_utils_py.generate_camera_frames import (
+    generate_camera_frames,
+)
+
+
+system_config_parser = SystemConfigParser()
+cameras_config = system_config_parser.get_cameras_config()
+
+
+def generate_launch_description():
+    frame_pair_params = [
+        {
+            "world_frame": "world",
+            "camera_frames": generate_camera_frames(cameras_config),
+        }
+    ]
+
+    camera_transforms_node = Node(
+        package="moveit_studio_agent",
+        executable="camera_transforms_node",
+        name="camera_transforms_node",
+        output="screen",
+        parameters=frame_pair_params,
+    )
+
+    return LaunchDescription(
+        [
+            camera_transforms_node,
+        ]
+    )
