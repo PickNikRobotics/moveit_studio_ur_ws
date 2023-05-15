44733b02a7332289fdfc46fcb60bcf489b2b68e8
Chance Cardona <chance.cardona@picknik.ai>
2023-05-15 13:37:15 -0600

    Incorporated cancel option with argparse into both scripts, removed explicit cancel script

--- src/moveit_studio_agent_examples/scripts/call_do_objective.py
+++ src/moveit_studio_agent_examples/scripts/call_do_objective.py
@@ -28,7 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
+import argparse
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -47,7 +47,7 @@ class DoObjectiveSequenceClient(Node):
         super().__init__("DoObjectiveSequence")
         self._action_client = ActionClient(self, DoObjectiveSequence, "do_objective")
 
-    def send_goal(self, objective_name):
+    def send_goal(self, objective_name, cancel):
         """
         Sends a DoObjectiveSequence Goal to the Objective Server via the node's Action Client.
 
@@ -59,6 +59,7 @@ class DoObjectiveSequenceClient(Node):
         """
         goal_msg = DoObjectiveSequence.Goal()
         goal_msg.objective_name = objective_name
+        self.cancel = cancel
         self._action_client.wait_for_server()
         self._send_goal_future = self._action_client.send_goal_async(goal_msg)
         self._send_goal_future.add_done_callback(self.goal_response_callback)
@@ -68,6 +69,8 @@ class DoObjectiveSequenceClient(Node):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info("Goal rejected.")
+
+            rclpy.shutdown()
             return
 
         self._goal_handle = goal_handle
@@ -75,6 +78,9 @@ class DoObjectiveSequenceClient(Node):
 
         get_result_future = goal_handle.get_result_async()
         get_result_future.add_done_callback(self.get_result_callback)
+        # Cancel after 2 seconds
+        if self.cancel:
+            self._timer = self.create_timer(2.0, self.cancel_goal)
 
     def get_result_callback(self, future):
         result = future.result().result
@@ -91,21 +97,47 @@ class DoObjectiveSequenceClient(Node):
 
         rclpy.shutdown()
 
+    def cancel_goal(self):
+        """
+        Cancels an Objective Server's DoObjectiveSequence Goal via the node's Action Client.
+
+        Returns:
+            future: a rclpy.task.Future that completes when the goal is canceled.
+        """
+        self.get_logger().info(f"Attempting to cancel goal.")
+        future = self._goal_handle.cancel_goal_async()
+        future.add_done_callback(self.cancel_goal_callback)
+        # Cancel the timer that this was a part of.
+        self._timer.cancel()
+        return future
+
+    def cancel_goal_callback(self, future):
+        cancel_response = future.result()
+        if cancel_response.goals_canceling:
+            self.get_logger().info("Goal successfully canceled.")
+        else:
+            self.get_logger().info("Goal failed to cancel.")
+
+        rclpy.shutdown()
+
 
 def main(args=None):
-    if len(sys.argv) < 2:
-        print(
-            "usage: ros2 run moveit_studio_agent_examples call_do_objective.py 'Objective Name'"
-        )
-    else:
-        rclpy.init(args=args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("objective_name", type=str, help="Name of Objective to run.")
+    parser.add_argument(
+        "--cancel",
+        action="store_true",
+        help="Optional boolean for if the objective should be automatically cancelled after a set amount of time.",
+    )
+    args = parser.parse_args()
 
-        client = DoObjectiveSequenceClient()
+    rclpy.init()
 
-        objective_name = sys.argv[1]
-        client.send_goal(objective_name)
+    client = DoObjectiveSequenceClient()
 
-        rclpy.spin(client)
+    future = client.send_goal(args.objective_name, args.cancel)
+
+    rclpy.spin(client)
 
 
 if __name__ == "__main__":
--- src/moveit_studio_agent_examples/scripts/call_do_objective_waypoint.py
+++ src/moveit_studio_agent_examples/scripts/call_do_objective_waypoint.py
@@ -28,7 +28,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
+import argparse
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
@@ -51,9 +51,9 @@ class DoObjectiveSequenceClient(Node):
         super().__init__("DoObjectiveSequence")
         self._action_client = ActionClient(self, DoObjectiveSequence, "do_objective")
 
-    def send_goal(self, waypoint_name="Behind"):
+    def send_goal(self, waypoint_name, cancel):
         """
-        Sends a DoObjectiveSequence Goal for "Move to Joint State" to the Objective Server via the node's Action Client.
+        Sends a DoObjectiveSequence Goal to the Objective Server via the node's Action Client.
 
         Args:
             waypoint_name: the (string) name of a waypoint to move to.
@@ -63,6 +63,7 @@ class DoObjectiveSequenceClient(Node):
         """
         goal_msg = DoObjectiveSequence.Goal()
         goal_msg.objective_name = "Move to Joint State"
+        self.cancel = cancel
 
         behavior_parameter = BehaviorParameter()
         behavior_parameter.behavior_namespaces.append("move_to_joint_state")
@@ -80,12 +81,18 @@ class DoObjectiveSequenceClient(Node):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info("Goal rejected.")
+
+            rclpy.shutdown()
             return
 
+        self._goal_handle = goal_handle
         self.get_logger().info("Goal accepted...")
 
         get_result_future = goal_handle.get_result_async()
         get_result_future.add_done_callback(self.get_result_callback)
+        # Cancel after 2 seconds
+        if self.cancel:
+            self._timer = self.create_timer(2.0, self.cancel_goal)
 
     def get_result_callback(self, future):
         result = future.result().result
@@ -102,21 +109,47 @@ class DoObjectiveSequenceClient(Node):
 
         rclpy.shutdown()
 
+    def cancel_goal(self):
+        """
+        Cancels an Objective Server's DoObjectiveSequence Goal via the node's Action Client.
+
+        Returns:
+            future: a rclpy.task.Future that completes when the goal is canceled.
+        """
+        self.get_logger().info(f"Attempting to cancel goal.")
+        future = self._goal_handle.cancel_goal_async()
+        future.add_done_callback(self.cancel_goal_callback)
+        # Cancel the timer that this was a part of.
+        self._timer.cancel()
+        return future
+
+    def cancel_goal_callback(self, future):
+        cancel_response = future.result()
+        if cancel_response.goals_canceling:
+            self.get_logger().info("Goal successfully canceled.")
+        else:
+            self.get_logger().info("Goal failed to cancel.")
+
+        rclpy.shutdown()
+
 
 def main(args=None):
-    if len(sys.argv) < 2:
-        print(
-            "usage: ros2 run moveit_studio_agent_examples call_do_objective_waypoint.py 'Waypoint Name'"
-        )
-    else:
-        rclpy.init(args=args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("waypoint_name", type=str, help="Name of waypoint to move to.")
+    parser.add_argument(
+        "--cancel",
+        action="store_true",
+        help="Optional boolean for if the objective should be automatically cancelled after a set amount of time.",
+    )
+    args = parser.parse_args()
 
-        client = DoObjectiveSequenceClient()
+    rclpy.init()
 
-        waypoint_name = sys.argv[1]
-        client.send_goal(waypoint_name)
+    client = DoObjectiveSequenceClient()
 
-        rclpy.spin(client)
+    future = client.send_goal(args.waypoint_name, args.cancel)
+
+    rclpy.spin(client)
 
 
 if __name__ == "__main__":
