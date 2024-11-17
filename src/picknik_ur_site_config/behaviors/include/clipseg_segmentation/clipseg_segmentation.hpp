759711799bf294c834d4980e7f4c4b37d6e9e783
Paul Gesel <paul.gesel@picknik.ai>
2024-11-17 15:20:19 -0700

    add clipseg behavior
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

---
+++ src/picknik_ur_site_config/behaviors/include/clipseg_segmentation/clipseg_segmentation.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+
+#include <moveit_studio_behavior_interface/async_behavior_base.hpp>
+#include <moveit_pro_ml/onnx_clipseg.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <fmt/format.h>
+
+namespace custom_behaviors
+{
+/**
+ * @brief Segment an image using the SAM 2 model
+ */
+class ClipSegSegmentation : public moveit_studio::behaviors::AsyncBehaviorBase
+{
+public:
+/**
+   * @brief Constructor for the clipseg_segmentation behavior.
+   * @param name The name of a particular instance of this Behavior. This will be set by the behavior tree factory when this Behavior is created within a new behavior tree.
+   * @param config This contains runtime configuration info for this Behavior, such as the mapping between the Behavior's data ports on the behavior tree's blackboard. This will be set by the behavior tree factory when this Behavior is created within a new behavior tree.
+   * @details An important limitation is that the members of the base Behavior class are not instantiated until after the initialize() function is called, so these classes should not be used within the constructor.
+   */
+ ClipSegSegmentation(const std::string& name, const BT::NodeConfiguration& config,
+                                     const std::shared_ptr<moveit_studio::behaviors::BehaviorContext>& shared_resources);
+
+ /**
+  * @brief Implementation of the required providedPorts() function for the sam2_segmentation Behavior.
+  * @details The BehaviorTree.CPP library requires that Behaviors must implement a static function named providedPorts() which defines their input and output ports. If the Behavior does not use any ports, this function must return an empty BT::PortsList.
+  * This function returns a list of ports with their names and port info, which is used internally by the behavior tree.
+  * @return sam2_segmentation does not use expose any ports, so this function returns an empty list.
+  */
+ static BT::PortsList providedPorts();
+
+ /**
+  * @brief Implementation of the metadata() function for displaying metadata, such as Behavior description and
+  * subcategory, in the MoveIt Studio Developer Tool.
+  * @return A BT::KeyValueVector containing the Behavior metadata.
+  */
+ static BT::KeyValueVector metadata();
+
+protected:
+  tl::expected<bool, std::string> doWork() override;
+
+
+private:
+ void set_onnx_from_ros_image(const sensor_msgs::msg::Image& image_msg);
+ void set_ros_image_from_onnx(const moveit_pro_ml::ONNXImage& onnx_image);
+
+ std::shared_ptr<moveit_pro_ml::ClipSeg> clipseg_;
+ moveit_pro_ml::ONNXImage onnx_image_;
+ sensor_msgs::msg::Image image_;
+ 
+  /** @brief Classes derived from AsyncBehaviorBase must implement getFuture() so that it returns a shared_future class member */
+  std::shared_future<tl::expected<bool, std::string>>& getFuture() override
+  {
+    return future_;
+  }
+
+  /** @brief Classes derived from AsyncBehaviorBase must have this shared_future as a class member */
+  std::shared_future<tl::expected<bool, std::string>> future_;
+
+};
+}  // namespace sam2_segmentation
---
+++ src/picknik_ur_site_config/behaviors/src/clipseg_segmentation.cpp
@@ -0,0 +1,136 @@
+#include <spdlog/spdlog.h>
+#include "opencv2/core.hpp"
+
+#include <clipseg_segmentation/clipseg_segmentation.hpp>
+#include <moveit_pro_ml/onnx_utils.hpp>
+
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <moveit_studio_vision_msgs/msg/mask2_d.hpp>
+#include <moveit_studio_behavior_interface/get_required_ports.hpp>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace
+{
+  constexpr auto kPortImage = "image";
+  constexpr auto kPortImageDefault = "{image}";
+  constexpr auto kPortPrompt = "prompt";
+  constexpr auto kPortPromptDefault = "{prompt}";
+  constexpr auto kPortMasks = "masks2d";
+  constexpr auto kPortMasksDefault = "{masks2d}";
+} // namespace
+
+namespace custom_behaviors
+{
+  ClipSegSegmentation::ClipSegSegmentation(const std::string& name, const BT::NodeConfiguration& config,
+                                     const std::shared_ptr<moveit_studio::behaviors::BehaviorContext>& shared_resources)
+    : moveit_studio::behaviors::AsyncBehaviorBase(name, config, shared_resources)
+  {
+    std::filesystem::path package_path = ament_index_cpp::get_package_share_directory("picknik_ur_site_config");
+    const std::filesystem::path clip_model_onnx_file = package_path / "models" / "clip.onnx";
+    const std::filesystem::path clipseg_model_onnx_file = package_path / "models" / "clipseg.onnx";
+    clipseg_ = std::make_shared<moveit_pro_ml::ClipSeg>(clip_model_onnx_file, clipseg_model_onnx_file);
+  }
+
+  BT::PortsList ClipSegSegmentation::providedPorts()
+  {
+    return {
+      BT::InputPort<sensor_msgs::msg::Image>(kPortImage, kPortImageDefault,
+                                             "The Image to run segmentation on."),
+      BT::InputPort<std::string>(kPortPrompt, kPortPromptDefault,
+                                                                   "The input prompt as a <code>std::string</code> to be used for segmentation."),
+
+  BT::OutputPort<std::vector<moveit_studio_vision_msgs::msg::Mask2D>>(kPortMasks, kPortMasksDefault,
+                                                                   "The masks contained in a vector of <code>moveit_studio_vision_msgs::msg::Mask2D</code> messages.")
+
+
+    };
+  }
+
+  void ClipSegSegmentation::set_onnx_from_ros_image(const sensor_msgs::msg::Image& image_msg)
+  {
+    onnx_image_.shape = {image_msg.height, image_msg.width, 3};
+    onnx_image_.data.resize(image_msg.height * image_msg.width * 3);
+    for (size_t i = 0; i < onnx_image_.data.size(); ++i)
+    {
+      onnx_image_.data[i] = static_cast<float>(image_msg.data[i]) / 255.0f;
+    }
+    onnx_image_ = moveit_pro_ml::permute_image_data(onnx_image_);
+  }
+
+
+  void ClipSegSegmentation::set_ros_image_from_onnx(const moveit_pro_ml::ONNXImage& onnx_image)
+  {
+    image_.height = static_cast<uint32_t>(onnx_image.shape[0]);
+    image_.width = static_cast<uint32_t>(onnx_image.shape[1]);
+    image_.encoding = "mono8";
+    image_.data.resize(image_.height * image_.width);
+    image_.step = image_.width;
+    for (size_t i = 0; i < image_.data.size(); ++i)
+    {
+      image_.data[i] = onnx_image.data[i]>0.5 ? 255: 0;
+    }
+
+    cv::Mat masks_scaled(image_.height, image_.width, CV_8UC1, image_.data.data());
+    cv::imwrite("/tmp/mask.png", masks_scaled); // TODO delete this
+
+   }
+
+
+  tl::expected<bool, std::string> ClipSegSegmentation::doWork()
+  {
+    const auto ports = moveit_studio::behaviors::getRequiredInputs(getInput<sensor_msgs::msg::Image>(kPortImage),
+                                                                   getInput<std::vector<
+                                                                     std::string>>(kPortPrompt));
+
+    // Check that all required input data ports were set.
+    if (!ports.has_value())
+    {
+      auto error_message = fmt::format("Failed to get required values from input data ports:\n{}", ports.error());
+      return tl::make_unexpected(error_message);
+    }
+    const auto& [image_msg, prompt] = ports.value();
+
+    if (image_msg.encoding != "rgb8")
+    {
+      auto error_message = fmt::format("Invalid image message format. Expected `rgb8` got :\n{}", image_msg.encoding);
+      return tl::make_unexpected(error_message);
+    }
+
+    // create ONNX formatted image tensor from ROS image
+    set_onnx_from_ros_image(image_msg);
+    std::vector<std::string> prompts = {prompt};
+    try
+    {
+      onnx_image_.shape = {1, onnx_image_.shape[0], onnx_image_.shape[1], onnx_image_.shape[2]};
+      auto logits = clipseg_->predict(onnx_image_, prompts);
+      logits.shape = {logits.shape[1], logits.shape[2]};
+      logits = moveit_pro_ml::resize_image(logits, {image_msg.height, image_msg.width});
+
+      moveit_studio_vision_msgs::msg::Mask2D our_mask;
+      image_.header = image_msg.header;
+      set_ros_image_from_onnx(logits);
+      our_mask.pixels = image_;
+      our_mask.x = 0;
+      our_mask.y = 0;
+      setOutput<std::vector<moveit_studio_vision_msgs::msg::Mask2D>>(kPortMasks, {our_mask});
+    }
+    catch (std::invalid_argument& e)
+    {
+      auto error_message = fmt::format("Invalid argument: {}", e.what());
+      return tl::make_unexpected(error_message);
+    }
+
+    return true;
+  }
+
+  BT::KeyValueVector ClipSegSegmentation::metadata()
+  {
+    return {
+      {
+        "description",
+        "Segments a ROS image message using the text prompt provided as a <code>std::string</code>."
+      }
+    };
+  }
+} // namespace sam2_segmentation
